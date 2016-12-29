a84f8f52a5c233771efd340ab524fa5378b2dac8
眀覺 <xxlsyq@foxmail.com>
2016-12-29 20:48:24 +0800

    Update ex04.py

--- ex04.py
+++ ex04.py
@@ -12,6 +12,6 @@ print "There are", cars, "cars availale."
 print "There are only", drivers, "drivers availale."
 print "There will be", cars_not_driven, "empty cars today."
 print "We can transport", carpool_capacity, "people today."
-print "We have", passengers,"to carpool today."
+print "We have", passengers, "to carpool today."
 print "We need to put about", average_passengers_per_car, "in each car."
 
